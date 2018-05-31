7791defaa63646d6ec0388067c990aadae2f3183
Yibo Guo <yig004@eng.ucsd.edu>
2018-05-31 16:46:47 -0700

    Added more benchmark in script and most hosts.

--- script/pssh_node.sh
+++ script/pssh_node.sh
@@ -20,14 +20,58 @@ if [ $index -lt 0 ]; then
     exit 2
 fi
 
-next=$(echo "($index + 1) % $host_count" | bc)
+curr_index=$index
+curr_host="${host_list[$curr_index]}"
+next_index=$(echo "($curr_index + 1) % $host_count" | bc)
+next_host="${host_list[$next_index]}"
 
 # Launch
 cd ../build
-echo "launch with option --host=$host --hosts=$hosts --index=$index --next=$next"
+echo "launch with option --hosts=$hosts --host=$curr_host --index=$curr_index --next-index=$next_index --next-host=$next_host"
 
-#echo "test" > "${host_list[$index]}_to_${host_list[$ndext]}.server.out"
-ib_write_bw -F -R -D 20s --report_gbits > "${host_list[$index]}_to_${host_list[$next]}.server.out" &
+# udaddy test
+: '
+udaddy &
+pid=$!
+udaddy -s $next_host
+wait $pid
+#'
+
+# rdma_server test
+: '
+rdma_server &
+pid=$!
+rdma_client -s $next_host
+wait $pid
+#'
+
+# Mallenox benchmark tool
+
+: '
+client=$curr_host
+server=$next_host
+client_log="${client}_to_${server}.client.out"
+server_log="${client}_to_${server}.server.out"
+duration="5s"
+ib_flags="-F -R -D $duration --report_gbits"
+
+echo "client_log=$client_log, server_log=$server_log, duration=$duration, ib_flags=$ib_flags"
+#'
+
+# ib_read_bw test
+: '
+ib_read_bw $ib_flags > $server_log &
+pid=$!
 sleep 1
-ib_write_bw -F -R -D 20s --report_gbits "${host_list[$next]}" | tee "${host_list[$index]}_to_${host_list[$next]}.client.out" &
+ib_read_bw $ib_flags $server | tee $client_log
+wait $pid
+#'
 
+# ib_write_bw test
+: '
+ib_write_bw $ib_flags > $server_log &
+pid=$!
+sleep 1
+ib_write_bw $ib_flags $server | tee $client_log
+wait $pid
+#'
