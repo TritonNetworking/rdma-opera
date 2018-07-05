c84defc428f298dbc0910387318b085827e50e69
Yibo Guo <yig004@eng.ucsd.edu>
2018-07-05 09:51:26 -0700

    Fixed minor bugs and adjusted measurement.

--- script/mpi-launch.sh
+++ script/mpi-launch.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 # Config
-execname=$HPCX_MPI_TESTS_DIR/examples/hello_c
+#execname=~/Source/rotornet-mpi/rlb_v1/rotor_test
+execname=../build/mpi_exec
 hostfile=hosts.config
 hosts=$(cat $hostfile| paste -s -d "," -)
 np=$(cat $hostfile | wc -l)
 
 # From https://community.mellanox.com/docs/DOC-3076#jive_content_id_Running_MPI
+#:'
 HCAS=mlx5_0:1
-
-FLAGS+="--host $hosts "
 FLAGS+="-mca btl_openib_warn_default_gid_prefix 0 "
 FLAGS+="-mca btl_openib_warn_no_device_params_found 0 "
 FLAGS+="--report-bindings --allow-run-as-root -bind-to core "
@@ -18,11 +18,28 @@ FLAGS+="-mca coll_fca_enable 0 -mca coll_hcoll_enable 0 "
 FLAGS+="-mca mtl_mxm_np 0 -x MXM_TLS=ud,shm,self -x MXM_RDMA_PORTS=$HCAS "
 FLAGS+="-x MXM_LOG_LEVEL=ERROR -x MXM_IB_PORTS=$HCAS "
 FLAGS+="-x MXM_IB_MAP_MODE=round-robin -x MXM_IB_USE_GRH=y "
+#'
 
 # From Max
 FLAGS+="-mca btl_openib_receive_queues P,65536,256,192,128:S,128,256,192,128:S,2048,1024,1008,64:S,12288,1024,1008,64:S,65536,1024,1008,64 "
 
+# Executable flags
+l=1024
+#limit=1024
+limit=$((1024*1024*1024))
+count=1
+verb="write"
+mode="throughput"
+warmup=0
+mr_count=1
+
 # Launch MPI job
 set -x
-mpirun -np $np --host $hosts $FLAGS $execname
+while [[ $l -le $limit ]]; do
+    echo "Length = $l ..."
+    execflags="-b $l -r $count -v $verb -m $mode -w $warmup --mr_count=$mr_count"
+    mpirun -np $np --host $hosts $FLAGS $execname $execflags
+    (( l *= 2 ))
+    echo ""
+done
 
--- src/mpi_main.c
+++ src/mpi_main.c
@@ -1,13 +1,42 @@
-#include <time.h>
-#include <stdio.h>
-#include <stdlib.h>
+// MPI tool
+
+#define _GNU_SOURCE
 
 #include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-#include "dccs_util.h"
+#include "dccs_utils.h"
 
-#define MESSAGE_SIZE 65536
-#define MESSAGE_COUNT 1000
+#define REPEAT 10
+
+uint64_t clock_rate = 0;    // Clock ticks per second
+
+int verify_checksum(const void *buf, size_t buffer_size, int rank, int size) {
+    unsigned char digest[SHA_DIGEST_LENGTH];
+    sha1sum(buf, buffer_size, digest);
+    char *digest_hex = bin_to_hex_string(digest, SHA_DIGEST_LENGTH);
+    if (rank == 0) {
+        //log_info("Verifying checksum ...\n");
+        MPI_Status status;
+        for (size_t src = 1; src < size; src++) {
+            unsigned char remote_digest[SHA_DIGEST_LENGTH];
+            MPI_Recv(remote_digest, SHA_DIGEST_LENGTH, MPI_CHAR, src, 0, MPI_COMM_WORLD, &status);
+            if (strncmp((const void *)digest, (const void *)remote_digest, SHA_DIGEST_LENGTH) != 0) {
+                log_error("Incorrect SHA sum from rank %d.\n", src);
+                return -1;
+            }
+        }
+
+        //log_info("Checksum verified.\n");
+    } else {
+        MPI_Send(digest, SHA_DIGEST_LENGTH, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+    }
+
+    free(digest_hex);
+    return 0;
+}
 
 int run(int size, int rank, struct dccs_parameters params) {
     int rv = 0;
@@ -17,45 +46,77 @@ int run(int size, int rank, struct dccs_parameters params) {
     size_t bytes_sent = 0;
     size_t buffer_size = params.length * params.count;
 
-    if (rank == 0) {
-        MPI_Request requests[(size - 1) * params.count];
-        MPI_Status statuses[(size - 1) * params.count];
-        buf = malloc_random(buffer_size);
+    buf = malloc_random(buffer_size);
 
-        MPI_Barrier(MPI_COMM_WORLD);
+    //MPI_Barrier(MPI_COMM_WORLD);
+    //start = get_cycles();
 
-        start = get_cycles();
-        for (int n = 0; n < params.count; n++) {
-            sendbuf = buf + n * params.length;
-            for (int dest = 1; dest < size; dest++) {
-                MPI_Isend(sendbuf, params.length, MPI_Byte, dest, 0, MPI_COMM_WORLD, requests + n * (size - 1) + dest);
+    for (size_t r = 0; r < REPEAT; r++) {
+        //buf = malloc_random(buffer_size);
+
+        int done = 0;
+        bytes_sent = 0;
+        if (rank == 0) {
+            MPI_Request requests[(size - 1) * params.count];
+
+            MPI_Barrier(MPI_COMM_WORLD);
+            start = get_cycles();
+            for (size_t n = 0; n < params.count; n++) {
+                sendbuf = (void *)((uint8_t *)buf + n * params.length);
+                for (size_t dest = 1; dest < size; dest++) {
+                    MPI_Isend(sendbuf, params.length, MPI_BYTE, dest, 0, MPI_COMM_WORLD, requests + n * (size - 1) + dest - 1);
+                    //MPI_Send(sendbuf, params.length, MPI_BYTE, dest, 0, MPI_COMM_WORLD);
+                    bytes_sent += params.length;
+                }
+            }
+
+            //MPI_Waitall((size - 1) * params.count, requests, MPI_STATUSES_IGNORE);
+            while (!done) {
+                MPI_Testall((size - 1) * params.count, requests, &done, MPI_STATUSES_IGNORE);
+            }
+
+            end = get_cycles();
+        } else {
+            int source = 0;
+            MPI_Request requests[params.count];
+
+            MPI_Barrier(MPI_COMM_WORLD);
+            start = get_cycles();
+            for (size_t n = 0; n < params.count; n++) {
+                recvbuf= (void *)((uint8_t *)buf + n * params.length);
+                MPI_Irecv(recvbuf, params.length, MPI_BYTE, source, 0, MPI_COMM_WORLD, requests + n);
+                //MPI_Recv(recvbuf, params.length, MPI_BYTE, source, 0, MPI_COMM_WORLD, &status);
                 bytes_sent += params.length;
             }
+
+            //MPI_Waitall(params.count, requests, MPI_STATUSES_IGNORE);
+            while (!done) {
+                MPI_Testall(params.count, requests, &done, MPI_STATUSES_IGNORE);
+            }
+
+            end = get_cycles();
         }
 
-        MPI_Waitall(params.count, requests, &statuses);
-        end = get_cycles();
-    } else {
-        MPI_Status status;
-        buf = malloc(buffer_size);
+        double elapsed = (double)(end - start) / clock_rate;
+        double elapsed_usec = elapsed * 1e6;
+        double throughput_gbits = bytes_sent * 8 / elapsed / (1024 * 1024 * 1024);
+        if (rank == 1)
+            log_info("rank = %d, bytes transferred = %zu, elapsed = %.3fµsec, throughput = %.3f gbits.\n", rank, bytes_sent, elapsed_usec, throughput_gbits);
 
-        MPI_Barrier(MPI_COMM_WORLD);
-        start = get_cycles();
-        for (int n = 0; n < params.count; n++) {
-            recvbuf = buf + n * params.length;
-            MPI_Recv(recvbuf, params.length, MPI_Byte, source, 0, MPI_COMM_WORLD, &status);
-            bytes_sent += params.length;
-        }
-
-        end = get_cycles();
+        //verify_checksum(buf, buffer_size, rank, size);
+        //free(buf);
     }
 
+    verify_checksum(buf, buffer_size, rank, size);
     free(buf);
 
+/*
+    end = get_cycles();
     double elapsed = (double)(end - start) / clock_rate;
-    double elapsed_µsec = elapsed * 1e6;
-    double throughput_gbits = bytes_sent * 8 / elapsed / 1e9;
-    log_info("rank = %d, bytes sent = %zu, elapsed = %.3fµsec, throughput = %.3f gbits.\n", rank, bytes_sent, elapsed_µsec, throughput_gbits);
+    double elapsed_usec = elapsed * 1e6;
+    double throughput_gbits = bytes_sent * 8 / elapsed / (1024 * 1024 * 1024);
+    log_info("rank = %d, bytes transferred = %zu, elapsed = %.3fµsec, throughput = %.3f gbits.\n", rank, bytes_sent, elapsed_usec, throughput_gbits);
+*/
 
     return rv;
 }
