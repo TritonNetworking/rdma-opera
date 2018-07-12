0d501da9fc87e950604edb8c27d271cda78b0cbe
Yibo Guo <yig004@eng.ucsd.edu>
2018-07-11 18:21:20 -0700

    Added option to plot min/max in addition to avg.

--- script/process_mpi_result.py
+++ script/process_mpi_result.py
@@ -8,8 +8,8 @@ import matplotlib.pyplot as plt
 parser = argparse.ArgumentParser()
 parser.add_argument('-l', '--logfile', required=True, help='Log file')
 parser.add_argument('-m', '--metric', required=True, choices=[ 'latency', 'throughput' ], help='Which metric to plot, "latency" or "throughput"')
+parser.add_argument('-s', '--stats', required=True, choices=[ 'avg', 'min', 'max' ], nargs='+', help='Which stat to print, "avg", "min", or "max"')
 args = parser.parse_args()
-
 
 min_bytes = 0
 max_rank = 0
@@ -50,7 +50,7 @@ with open(args.logfile) as f:
         entries[bytes].append([round, rank, elapsed, throughput])
 
 ranks = max_rank + 1
-avgs = {}
+data = {}
 stdevs = {}
 for k, v in entries.iteritems():
     bytes = k
@@ -58,36 +58,53 @@ for k, v in entries.iteritems():
     t = {}
     for [round, rank, elapsed, throughput] in v:
         # Use round if 1-N and N-1, and (round, rank) for N-N
-        if (round, rank) not in t:
-            t[(round, rank)] = 0
         if args.metric == 'latency':
             metric = elapsed
         elif args.metric == 'throughput':
             metric = throughput
+
+        if (round, rank) not in t:
+            t[(round, rank)] = 0
         t[(round, rank)] += metric
-    avg = sum(t.itervalues()) / len(t)
+
     # print [v for v in t.itervalues()]
+    avgv = sum(t.itervalues()) / len(t)
+    minv = min(t.itervalues())
+    maxv = max(t.itervalues())
     stdev = np.std([v for v in t.itervalues()])
-    avgs[bytes] = avg
+    data[bytes] = (minv, avgv, maxv)
     stdevs[bytes] = stdev
-    # print bytes, avg, stdev
+    # print bytes, avgv, minv, maxv, stdev
     # for round, throughput in t.iteritems():
         # print bytes, round, throughput
 
-N = len(avgs)
-np_x = np.array([bytes for bytes, _ in sorted(avgs.iteritems())])
-np_avg = np.array([avg for _, avg in sorted(avgs.iteritems())])
+N = len(data)
+np_x = np.array([bytes for bytes, _ in sorted(data.iteritems())])
+np_avg = np.array([item[1] for _, item in sorted(data.iteritems())])
 np_std = np.array([stdev for _, stdev in sorted(stdevs.iteritems())])
-plt.errorbar(np_x, np_avg, np_std, marker='.', linewidth=0.75)
+np_min = np.array([item[0] for _, item in sorted(data.iteritems())])
+np_max = np.array([item[2] for _, item in sorted(data.iteritems())])
 
-plt.xscale('log', basex=2)
-plt.ylim(0, 100)
+if 'avg' in args.stats:
+    plt.errorbar(np_x, np_avg, np_std, label='avg', marker='.', linewidth=0.75)
+if 'min' in args.stats:
+    plt.plot(np_x, np_min, label='min', marker=".", linewidth=0.75)
+if 'max' in args.stats:
+    plt.plot(np_x, np_max, label='max', marker=".", linewidth=0.75)
 
 plt.xlabel('Message size per node (bytes)')
-plt.ylabel('Throughput (Gbps)')
+plt.xscale('log', basex=2)
+plt.xlim(min(np_x), max(np_x))
+if args.metric == 'latency':
+    plt.ylabel('Latency (usec)')
+    #plt.ylim(0, 1000)
+else:
+    plt.ylabel('Throughput (Gbps)')
+    plt.ylim(0, 100)
 
 name = os.path.basename(args.logfile).split('.')[0]
 plt.title(name + ' Traffic, 2 + 8 iterations')
+plt.legend()
 
 plt.show()
 
