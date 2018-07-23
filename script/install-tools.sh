be65e8d0690ccd204a56e7f0e62218178fb3fc26
Yibo Guo <yig004@eng.ucsd.edu>
2018-07-22 20:42:07 -0700

    Added support for using huge pages.

--- script/install-tools.sh
+++ script/install-tools.sh
@@ -4,4 +4,5 @@ sudo apt-get install -y iperf perftest
 sudo apt-get install -y bc cmake libssl-dev pssh
 sudo apt-get install -y dos2unix
 sudo apt-get install -y cpufrequtils
+sudo apt-get install -y libhugetlbfs-dev
 
