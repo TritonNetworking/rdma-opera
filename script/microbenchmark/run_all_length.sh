5891765fa82d69d1dae6670b00f99a6f23d205c4
Yibo Guo <yig004@eng.ucsd.edu>
2018-08-30 13:47:28 -0700

    Changed script to send large RDMA traffic.

--- script/microbenchmark/run_all_length.sh
+++ script/microbenchmark/run_all_length.sh
@@ -6,19 +6,20 @@ fi
 
 source ./config
 
-l=1
-limit=8388608
+l=$((1*1024*1024*1024))
+limit=$((1*1024*1024*1024))
 
-count=1000
+count=1
 verb="read"
-mode="throughput"
-repeat=1
+mode="latency"
+repeat=100
 warmup=0
 mr_count=1
 
 server="$1"
 execpath=$RDMA_BENCH_EXECPATH
 
+set -x
 while [[ $l -le $limit ]]; do
     echo "Length = $l ..."
     $execpath -b $l -c $count -v $verb -m $mode -r $repeat -w $warmup --mr_count=$mr_count $server
