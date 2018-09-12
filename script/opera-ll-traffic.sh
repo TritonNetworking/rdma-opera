03f6b23dc2219badf8213b37d4676289fe0eec16
Yibo Guo <yig004@eng.ucsd.edu>
2018-09-11 17:40:31 -0700

    Added script to run opera low latency traffic.

---
+++ script/opera-ll-traffic.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env bash
+
+min_host_num=38
+max_host_num=44
+
+curr_host=$(hostname -s)
+curr_host_num=${curr_host:4:2}
+
+# Run configuration
+LATENCY_GAP=1000    # in µs
+ITERS=$((60 * 1000))
+BLOCKSIZE=2
+WAIT=false
+
+COMMON_FLAG="-R -U -s $BLOCKSIZE -n $ITERS --latency_gap $LATENCY_GAP"
+
+# Program settings
+LOG_DIR=$HOME/opera.logs
+PROGRAM=~/Source/examples/perftest/ib_send_lat
+PORT_PREFIX="100"
+HOST_PREFIX="b09-"
+
+# hostname to IP mapping
+declare -A host2ip
+host2ip["b09-38"]="10.0.0.4"
+host2ip["b09-40"]="10.0.0.6"
+host2ip["b09-42"]="10.0.0.3"
+host2ip["b09-44"]="10.0.0.5"
+
+#killall $(basename $PROGRAM)
+
+# Clean log directory
+mkdir -p $LOG_DIR
+rm -f "$LOG_DIR/*"
+
+# Each host connects to every host behind it and accepts connections from every host before it.
+
+# Launch servers
+echo "Launching servers ..."
+for (( num = $min_host_num; num < $curr_host_num; num += 2 ))
+do
+    client_num=$num
+    server_num=$curr_host_num
+    conn_name="$server_num-$client_num"
+    port="$PORT_PREFIX$client_num"
+    logfile="$LOG_DIR/$conn_name.log"
+    echo "Launching server $conn_name ..."
+    flags="$COMMON_FLAG -p $port"
+    if $WAIT; then
+        $PROGRAM $flags > $logfile &
+        spids[${num}]=$!
+    else
+        nohup $PROGRAM $flags > $logfile &
+    fi
+done
+echo
+
+echo "Sleeping for 1s ..."
+echo
+sleep 1
+
+# Launch clients
+echo "Launching clients ..."
+for (( num = $((curr_host_num + 2)); num <= $max_host_num; num += 2 ))
+do
+    client_num=$curr_host_num
+    server_num=$num
+    server_name="$HOST_PREFIX$server_num"
+    server=${host2ip[$server_name]}
+    conn_name="$client_num-$server_num"
+    port="$PORT_PREFIX$client_num"
+    logfile="$LOG_DIR/$conn_name.log"
+    echo "Launching client $conn_name ..."
+    flags="$COMMON_FLAG -p $port $server"
+    if $WAIT; then
+        $PROGRAM $flags > $logfile &
+        cpids[${num}]=$!
+    else
+        nohup $PROGRAM $flags > $logfile &
+    fi
+done
+echo
+
+if $WAIT; then
+    for pid in ${spids[*]}; do
+        echo "Waiting for server process $pid ..."
+        wait $pid
+    done
+
+    for pid in ${cpids[*]}; do
+        echo "Waiting for client process $pid ..."
+        wait $pid
+    done
+fi
---
+++ script/pssh-launch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+hosts=$(cat hosts.config | paste -s -d " " -)
+DEFAULT_COMMAND='echo "Hello from $HOSTNAME"'
+command=${1:-$DEFAULT_COMMAND}
+
+command -v pssh >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    pssh=parallel-ssh
+else
+    pssh=pssh
+fi
+
+$pssh -i -H "$hosts" $command
