b7455954f83f9808e83b1f1cb0003fba71e7c94a
Yibo Guo <yig004@eng.ucsd.edu>
2018-10-26 10:29:31 -0700

    Updated IP configurations.

--- script/opera/opera-ll-traffic.sh
+++ script/opera/opera-ll-traffic.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")"
 
-min_host_num=30
+min_host_num=38
 max_host_num=44
 
 curr_host=$(hostname -s)
@@ -27,14 +27,10 @@ HOST_PREFIX="b09-"
 
 # hostname to IP mapping
 declare -A host2ip
-host2ip["b09-30"]="10.0.0.1"
-host2ip["b09-32"]="10.0.0.2"
-host2ip["b09-34"]="10.0.0.7"
-host2ip["b09-36"]="10.0.0.8"
-host2ip["b09-38"]="10.0.0.4"
-host2ip["b09-40"]="10.0.0.6"
+host2ip["b09-38"]="10.0.0.1"
+host2ip["b09-40"]="10.0.0.2"
 host2ip["b09-42"]="10.0.0.3"
-host2ip["b09-44"]="10.0.0.5"
+host2ip["b09-44"]="10.0.0.4"
 
 #killall $(basename $PROGRAM)
 
