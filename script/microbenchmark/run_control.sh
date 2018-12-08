5c0a9eb864a8b0f0ae8718da4ef08804fc9dab4f
Yibo Guo <nilyibo@gmail.com>
2018-12-07 17:14:07 -0800

    Increase message count to 8 per group.

--- script/microbenchmark/run_control.sh
+++ script/microbenchmark/run_control.sh
@@ -6,19 +6,20 @@ fi
 
 source ./config
 
-count=1
+count=8
 length=6
 mode="latency"
 verb="write"
-repeat=1000
+repeat=1000000
 warmup=0
 mr_count=1
 tos=32
+index=0
 
 server="$1"
 execpath=$CONTROL_SIGNAL_EXECPATH
 execflags=""
-execflags+="-b $length -c $count -v $verb -m $mode -r $repeat "
+execflags+="-b $length -c $count -v $verb -m $mode -r $repeat -i $index "
 execflags+="-w $warmup --mr_count=$mr_count --tos=$tos "
 execflags+="--verbose "
 
